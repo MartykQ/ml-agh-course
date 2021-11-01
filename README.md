c3bb38bebe6300ae240c96c5eed34a221072b20f
MartykQ <fmartyka16@gmail.com>
2021-11-01 10:13:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://ermlab.com/en/blog/nlp/polish-sentiment-analysis-using-keras-and-word2vec/
+
+https://github.com/Ermlab/pl-sentiment-analysis/blob/master/README.md
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+    return (
+        <div className="gradient-background">
+            <div className="main-box">
+                <Button variant="outlined">Hello world </Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
