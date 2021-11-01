efed6603be3e9f2ae44e7cc0be3b36f03bd0efcc
MartykQ <fmartyka16@gmail.com>
2021-11-01 14:22:17 +0100

    changed root source

--- server/sentiment_server/__init__.py
+++ server/sentiment_server/__init__.py
@@ -3,10 +3,10 @@ Main app module
 """
 
 import logging
-from sentiment_server.common.settings import LOGGING_FILE, LOGGING_FORMAT, DATA_FMT
+from server.sentiment_server.common.settings import LOGGING_FILE, LOGGING_FORMAT, DATA_FMT
 from flask import Flask
 from flask_cors import CORS
-from sentiment_server.routes.status_routes import status_routes
+from server.sentiment_server.routes.status_routes import status_routes
 
 logging.basicConfig(filename=LOGGING_FILE,
                     filemode='a',
