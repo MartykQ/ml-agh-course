f58af678043c375dc89f5ea7123d0f17972667c8
MartykQ <fmartyka16@gmail.com>
2021-11-03 19:15:38 +0100

    path fix

--- server/sentiment_server/common/settings.py
+++ server/sentiment_server/common/settings.py
@@ -8,11 +8,7 @@ DATA_FMT = '%y.%b.%d %H:%M:%S'
 
 ENV_FILE = './.env'
 
-if path.exists(ENV_FILE):
-    envir = Env()
-    envir.read_env()
-else:
-    print("Error .env file not found")
-    # sys.exit(1)
+envir = Env()
+envir.read_env()
 
 YOUTUBE_TOKEN = envir('YOUTUBE_TOKEN')
