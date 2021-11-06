f2a8e16bad708b7f93db547806051431620c7418
MartykQ <fmartyka16@gmail.com>
2021-11-06 15:22:46 +0100

    another ui update - styling  youtube

--- client/src/components/YoutubeSource/YoutubeSource.js
+++ client/src/components/YoutubeSource/YoutubeSource.js
@@ -2,32 +2,55 @@ import { chainPropTypes } from "@mui/utils";
 import axios from "../../constants";
 import { useState } from "react";
 import MyModal from "../MyModal/MyModal";
+import LoadingButton from "@mui/lab/LoadingButton";
+import TextField from "@mui/material/TextField";
+import SendIcon from "@mui/icons-material/Send";
+import "./YoutubeSource.css";
 
 const YoutubeSource = () => {
     const [comments, setComments] = useState([]);
-
+    const [inputText, setInputText] = useState("");
     const [modal, setModal] = useState(false);
     const ToggleModdle = () => setModal(!modal);
+    const [isLoading, setisLoading] = useState(false);
 
     const handleClick = () => {
-        console.log(process.env.ENV_VARIABLE);
+        setisLoading(true);
         axios
             .post("/youtube_comments", {
-                url: "https://www.youtube.com/watch?v=cqdVBhPp_xk",
+                url: inputText,
             })
             .then((res) => {
                 setComments(res["data"]);
+                setisLoading(false);
+                setModal(true);
             });
+    };
+
+    const handleTyping = (e) => {
+        setInputText(e.target.value);
     };
 
     return (
         <>
-            <MyModal show={modal} close={ToggleModdle}></MyModal>
-            <div>YT Source</div>
-            <div onClick={() => handleClick()}>click me!</div>
-            {comments.map((comment, index) => {
-                return <div key={index}>{comment}</div>;
-            })}
+            <MyModal show={modal} close={ToggleModdle}>
+                {" "}
+                {comments.map((comment, index) => {
+                    return <div key={index}>{comment}</div>;
+                })}
+            </MyModal>
+            <div className="inputButtonWrapper">
+                <TextField label="Youtube url" fullWidth={true} onChange={handleTyping} />
+                <LoadingButton
+                    endIcon={<SendIcon />}
+                    loading={isLoading}
+                    loadingPosition="end"
+                    variant="contained"
+                    onClick={() => handleClick()}
+                >
+                    Get comments
+                </LoadingButton>
+            </div>
         </>
     );
 };
