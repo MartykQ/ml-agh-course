93582fe053285d589c644426d98e9b568ebffc44
MartykQ <fmartyka16@gmail.com>
2021-11-07 10:16:49 +0100

    Added twitter integration

---
+++ client/src/components/TwitterSource/TwitterSource.js
@@ -0,0 +1,58 @@
+import { chainPropTypes } from "@mui/utils";
+import axios from "../../constants";
+import { useState } from "react";
+import MyModal from "../MyModal/MyModal";
+import LoadingButton from "@mui/lab/LoadingButton";
+import TextField from "@mui/material/TextField";
+import SendIcon from "@mui/icons-material/Send";
+import "./TwitterSource.css";
+
+const TwitterSource = () => {
+    const [comments, setComments] = useState([]);
+    const [inputText, setInputText] = useState("");
+    const [modal, setModal] = useState(false);
+    const ToggleModdle = () => setModal(!modal);
+    const [isLoading, setisLoading] = useState(false);
+
+    const handleClick = () => {
+        setisLoading(true);
+        axios
+            .post("/tweets", {
+                url: inputText,
+            })
+            .then((res) => {
+                setComments(res["data"]);
+                setisLoading(false);
+                setModal(true);
+            });
+    };
+
+    const handleTyping = (e) => {
+        setInputText(e.target.value);
+    };
+
+    return (
+        <>
+            <MyModal show={modal} close={ToggleModdle}>
+                {" "}
+                {comments.map((comment, index) => {
+                    return <div key={index}>{comment}</div>;
+                })}
+            </MyModal>
+            <div className="inputButtonWrapper">
+                <TextField label="Twitter url" fullWidth={true} onChange={handleTyping} />
+                <LoadingButton
+                    endIcon={<SendIcon />}
+                    loading={isLoading}
+                    loadingPosition="end"
+                    variant="contained"
+                    onClick={() => handleClick()}
+                >
+                    Get comments
+                </LoadingButton>
+            </div>
+        </>
+    );
+};
+
+export default TwitterSource;
