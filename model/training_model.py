aed3751309f34cfdfdcb748ae52eb9315f215e31
batotnv <bartosznguyenvan@gmail.com>
2021-11-08 09:50:23 +0100

    training model scipt amended

--- model/training_model.py
+++ model/training_model.py
@@ -48,3 +48,69 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.
 
 #stworzenie wektoru słów na bazie modelu z polskiej akademii nauk
 word2vec_model = gensim.models.KeyedVectors.load_word2vec_format('nkjp.txt', binary=False)
+
+embedding_matrix = word2vec_model.vectors
+
+top_words = embedding_matrix.shape[0]
+mxlen = 50
+nb_classes = 3
+
+#vocabulary index update
+tokenizer = Tokenizer(num_words=top_words)
+tokenizer.fit_on_texts(X_train)
+
+#texts_to_sequences Transforms each text in texts to a sequence of integers
+sequences_train = tokenizer.texts_to_sequences(X_train)
+sequences_test = tokenizer.texts_to_sequences(X_test)
+sequences_val = tokenizer.texts_to_sequences(X_val)
+
+word_index = tokenizer.word_index
+
+#Pads sequences to the same length.
+X_train = sequence.pad_sequences(sequences_train, maxlen=mxlen)
+X_test = sequence.pad_sequences(sequences_test, maxlen=mxlen)
+X_val = sequence.pad_sequences(sequences_val, maxlen=mxlen)
+
+#to categorical converts a class vector (integers) to binary class matrix.
+y_train = np_utils.to_categorical(y_train, nb_classes)
+y_test = np_utils.to_categorical(y_test, nb_classes)
+y_val = np_utils.to_categorical(y_val, nb_classes)
+
+#creating LST network model (kind of RNN)
+
+batch_size = 32
+nb_epoch = 3
+
+embedding_layer = Embedding(embedding_matrix.shape[0],
+                            embedding_matrix.shape[1],
+                            weights=[embedding_matrix],
+                            trainable=False)
+
+model = Sequential()
+model.add(embedding_layer)
+model.add(LSTM(128, recurrent_dropout=0.5, dropout=0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+#model.summary()
+
+
+#compile and train
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+rnn = model.fit(X_train, y_train, epochs= nb_epoch, batch_size=batch_size, shuffle=True, validation_data=(X_val, y_val))
+score = model.evaluate(X_val, y_val)
+
+
+#saving model
+
+# print('Save model...')
+# model.save('Models/finalsentimentmodel.h5')
+# print('Saved model to disk...')
+
+# print('Save Word index...')
+# output = open('Models/finalwordindex.pkl', 'wb')
+# pickle.dump(word_index, output)
+# print('Saved word index to disk...')
+
+
+
