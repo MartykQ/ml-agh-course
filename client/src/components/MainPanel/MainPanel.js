a4b09bc6c065df2e8d09357c6ea8b30fc3de059a
MartykQ <fmartyka16@gmail.com>
2021-11-13 19:11:32 +0100

    added translate version

--- client/src/components/MainPanel/MainPanel.js
+++ client/src/components/MainPanel/MainPanel.js
@@ -3,10 +3,11 @@ import { useState } from "react";
 import "./MainPanel.css";
 import LoadingButton from "@mui/lab/LoadingButton";
 import MyModal from "../MyModal/MyModal";
-
+import axios from "../../constants";
 import SendIcon from "@mui/icons-material/Send";
+import PredictedComment from "../PredictedComment/PredictedComment";
 
-const MainPanel = () => {
+const MainPanel = (props) => {
     const [inputText, setInputText] = useState("");
 
     const [modal, setModal] = useState(false);
@@ -18,6 +19,26 @@ const MainPanel = () => {
 
     const openModal = () => {
         setModal(true);
+    };
+
+    const [comments, setComments] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
+
+    const handleClick = () => {
+        setisLoading(true);
+
+        let _url = props.ibmModel ? "/ibm_single" : "/single";
+        console.log(_url);
+        axios
+            .post(_url, {
+                text: inputText,
+            })
+            .then((res) => {
+                console.log(res);
+                setComments(res["data"]);
+                setisLoading(false);
+                setModal(true);
+            });
     };
 
     return (
@@ -42,7 +63,7 @@ const MainPanel = () => {
                             loading={false}
                             loadingPosition="end"
                             variant="contained"
-                            onClick={openModal}
+                            onClick={handleClick}
                         >
                             Send
                         </LoadingButton>
@@ -50,7 +71,7 @@ const MainPanel = () => {
                 </div>
             </div>
             <MyModal show={modal} close={ToggleModdle}>
-                Tutaj bedzie ocenka opinii
+                <PredictedComment text={comments.text} label={comments.label} />
             </MyModal>
         </div>
     );
